4e8451c7249f88cd42067e74bcc39eee4735a62a
Larissa-29 <lravessoud@gmail.com>
2024-09-13 12:33:23 +0200

    Update analysis.R

--- analysis.R
+++ analysis.R
@@ -17,3 +17,24 @@ pwr.r.test(n=NULL, r=0.5, sig.level = 0.005, power = 0.9, alternative = c("two.s
 pwr.r.test(n=250, r=0.1, sig.level = 0.05, power = NULL, alternative = c("two.sided"))
 pwr.r.test(n=250, r=0.3, sig.level = 0.05, power = NULL, alternative = c("two.sided"))
 pwr.r.test(n=250, r=0.5, sig.level = 0.05, power = NULL, alternative = c("two.sided"))
+
+setwd("/Users/larissa-ravessoud/Documents/GitHub/GIT_2_LARISSA")
+
+mydata <- read.table("mydata.txt", header=T, sep="\t")
+
+View(mydata)
+dim(mydata)
+summary(mydata)
+head(mydata)
+
+mydata$Sex <- as.factor(mydata$Sex)
+mydata$Filter <- as.factor(mydata$Filter)
+summary(mydata)
+
+mydata <- subset(mydata, Filter == 0)
+nrow(mydata)
+
+t.test(mydata$Testosteron ~ mydata$Sex)
+plot(mydata$Testosteron ~ mydata$Sex)
+summary(mydata$Testosteron[mydata$Sex==0])
+summary(mydata$Testosteron[mydata$Sex==1])
