48668ee7acb9da8ddcb2ec690c6022ec9fb7d023
Larissa-29 <lravessoud@gmail.com>
2024-09-13 12:39:12 +0200

    Update analysis.R

--- analysis.R
+++ analysis.R
@@ -38,3 +38,29 @@ t.test(mydata$Testosteron ~ mydata$Sex)
 plot(mydata$Testosteron ~ mydata$Sex)
 summary(mydata$Testosteron[mydata$Sex==0])
 summary(mydata$Testosteron[mydata$Sex==1])
+
+mydata$Sex_ch <- factor(mydata$Sex, levels=c(0,1), labels=c("female", "male"))
+summary(mydata)
+
+summary(lm(Extraversion ~ Sex_ch, data=mydata))
+
+cor(mydata$EM_SD, mydata$EM_LD, use="pairwise")
+t.test(mydata$EM_SD, mydata$EM_LD)
+summary(mydata$EM_SD)
+summary(mydata$EM_LD)
+
+mydata$EM <- (mydata$EM_SD + mydata$EM_LD)/2
+
+plot(density(mydata$EM_SD), main="Compare EM SD and LD", frame.plot=F)
+lines(density(mydata$EM_LD), col="red")
+lines(density(mydata$EM), col="green")
+
+
+cor(mydata$fMRI_amy_neg_neu, mydata$fMRI_hipp_neg_neu)
+plot(mydata$fMRI_amy_neg_neu, mydata$fMRI_hipp_neg_neu, pch=19)
+abline(lm(mydata$fMRI_hipp_neg_neu ~ mydata$fMRI_amy_neg_neu))
+
+summary(lm(EM ~ Sex_ch + Extraversion + fMRI_hipp_neg_neu, data=mydata))
+
+
+
